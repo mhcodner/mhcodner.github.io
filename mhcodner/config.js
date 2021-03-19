96f5631d306aeb9be7ddacd2550f087849397e55
Michael Codner <mhcodner@gmail.com>
2021-03-19 16:45:19 +1100

    Add Gatsby template

---
+++ mhcodner/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  siteTitle: 'Michael Codner', // <title>
+  manifestName: 'Portfolio',
+  manifestShortName: 'mhcodner', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: '', // This path is subpath of your hosting https://domain/portfolio
+  authorName: 'Michael Codner',
+  heading: 'Engineer',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/mhcodner',
+    },
+    {
+      icon: 'fa-linkedin',
+      name: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/mhcodner/',
+    },
+  ],
+};
---
+++ mhcodner/src/components/PageFooter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function PageFooter() {
+  return (
+    <div id="footer">
+      <ul className="copyright">
+        <li>&copy; Michael Codner.</li>
+        <li>
+          Design: <a href="http://html5up.net">HTML5 UP</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
