dc00eb656f4eeea19700cb47a67963fa23261423
Michael Codner <mhcodner@gmail.com>
2021-08-19 22:36:44 +1000

    Change page description

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -44,7 +44,7 @@ class Layout extends Component {
             <Helmet
               title={data.site.siteMetadata.title}
               meta={[
-                { name: 'description', content: 'Eventually' },
+                { name: 'description', content: 'Michael Codner ePortfolio' },
                 { name: 'keywords', content: 'site, web' },
               ]}
             >
